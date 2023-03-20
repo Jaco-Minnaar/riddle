bc9a9236f50ea260395c824656c06ffb22931ca8
Jaco Minnaar <jaco@jacominnaar.com>
2023-03-20 21:26:36 +0200

    feat: add log filter

--- riddle_bin/src/logger.rs
+++ riddle_bin/src/logger.rs
@@ -2,21 +2,24 @@ use std::{fs::File, path::Path};
 
 use anyhow::{Context, Result};
 use log::LevelFilter;
-use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode, WriteLogger};
+use simplelog::{
+    ColorChoice, CombinedLogger, Config, ConfigBuilder, TermLogger, TerminalMode, WriteLogger,
+};
 
 pub fn create_logger(path: impl AsRef<Path>) -> Result<()> {
     let name = path.as_ref().display().to_string();
     let error_message = format!("Failed to create log file name: {name}");
+    let config = ConfigBuilder::new().add_filter_allow_str("riddle").build();
 
     let file = File::create(path).context(error_message)?;
     CombinedLogger::init(vec![
         TermLogger::new(
             LevelFilter::Info,
-            Config::default(),
+            config.clone(),
             TerminalMode::Mixed,
             ColorChoice::Auto,
         ),
-        WriteLogger::new(LevelFilter::max(), Config::default(), file),
+        WriteLogger::new(LevelFilter::max(), config, file),
     ])?;
 
     Ok(())
