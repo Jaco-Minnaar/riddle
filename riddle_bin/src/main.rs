801b732e20323dc11c2c45fe71bcbcd25367d1db
Jaco Minnaar <jaco@jacominnaar.com>
2023-06-05 20:47:25 +0200

    fix: add passing of tokens to client

--- riddle_bin/src/main.rs
+++ riddle_bin/src/main.rs
@@ -1,15 +1,18 @@
 mod logger;
 
-use riddle::app;
-
 use anyhow::Result;
+use riddle::app;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     logger::create_logger("riddle.log").expect("error creating logger");
 
-    let mut client = app::init_client().await?;
+    let discord_token = env::var("DISCORD_API_KEY")?;
+    let openai_token = env::var("OPENAI_API_KEY")?;
+
+    let mut client = app::init_client(&discord_token, openai_token).await?;
 
     if let Err(e) = client.start().await {
         log::error!("Error: {}", e);
